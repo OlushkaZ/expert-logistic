a6691067104755ea33caf0c289ef04635536293f
OlushkaZ <gincgo@yandex.ru>
2020-03-11 18:53:10 +0300

    slider - start

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,33 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+// var pageHeader = document.querySelector('.page-header');
+// var headerToggle = document.querySelector('.page-header__toggle');
+//
+// pageHeader.classList.remove('page-header--nojs');
+//
+// headerToggle.addEventListener('click', function () {
+//   if (pageHeader.classList.contains('page-header--closed')) {
+//     pageHeader.classList.remove('page-header--closed');
+//     pageHeader.classList.add('page-header--opened');
+//   } else {
+//     pageHeader.classList.add('page-header--closed');
+//     pageHeader.classList.remove('page-header--opened');
+//   }
+// });
 
-pageHeader.classList.remove('page-header--nojs');
+var thumbnailsSlides = document.querySelectorAll('.slides__thumbnails-item');
+var slides = document.querySelectorAll('.slides');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
-});
+var addThumbnailClickHandler = function (number) {
+  thumbnailsSlides[number].addEventListener('click', function () {
+    for (var j = 0; j < slides.length; j++) {
+      slides[j].classList.remove('slide-show');
+      thumbnailsSlides[j].classList.remove('slides__thumbnail-current');
+    }
+    slides[number].classList.add('slide-show');
+    thumbnailsSlides[number].classList.add('slides__thumbnail-current');
+  });
+};
+
+for (var i = 0; i < thumbnailsSlides.length; i++) {
+  addThumbnailClickHandler(i);
+}
