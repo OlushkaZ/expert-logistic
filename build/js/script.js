3e160b57414a9acc50eaa36eace864d66a87da7e
OlushkaZ <gincgo@yandex.ru>
2020-03-15 15:04:38 +0300

    refactoring 1

--- build/js/script.js
+++ build/js/script.js
@@ -14,33 +14,24 @@
 //   }
 // });
 
-var thumbnail = document.querySelectorAll('.slides__thumbnails');
 var thumbnailsSlides = document.querySelectorAll('.slides__thumbnails-item');
 var slides = document.querySelectorAll('.slides');
-var slidesContent = document.querySelectorAll('.slides__content-wrapper');
-console.dir(slidesContent);
-console.dir(thumbnail);
 
 var addThumbnailClickHandler = function (number) {
   thumbnailsSlides[number].addEventListener('click', function () {
-    if (slides) {
-      for (var j = 0; j < slides.length; j++) {
-        slides[j].classList.remove('slide-show');
-        slides[j].classList.remove('slides--opacity');
-        thumbnailsSlides[j].classList.remove('slides__thumbnail-current');
-      }
-      console.log(slidesContent[number].offsetHeight);
-      thumbnail.offsetTop = slidesContent[number].offsetHeight;
-      slides[number].classList.add('slide-show');
-      setTimeout(function () {
-        slides[number].classList.add('slides--opacity');
-      }, 100);
-      thumbnailsSlides[number].classList.add('slides__thumbnail-current');
+    for (var j = 0; j < slides.length; j++) {
+      slides[j].classList.remove('slide-show');
+      slides[j].classList.remove('slides--opacity');
+      thumbnailsSlides[j].classList.remove('slides__thumbnail-current');
     }
+    slides[number].classList.add('slide-show');
+    setTimeout(function () {
+      slides[number].classList.add('slides--opacity');
+    }, 100);
+    thumbnailsSlides[number].classList.add('slides__thumbnail-current');
   });
 };
-if (thumbnailsSlides) {
-  for (var i = 0; i < thumbnailsSlides.length; i++) {
-    addThumbnailClickHandler(i);
-  }
+
+for (var i = 0; i < thumbnailsSlides.length; i++) {
+  addThumbnailClickHandler(i);
 }
